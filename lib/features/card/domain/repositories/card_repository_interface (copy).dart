83b2d4c0113c3824688e871730d0fe96e9ddf53a
giovaneneves7 <autornekoyasha@gmail.com>
2024-09-21 20:35:00 -0300

    dsrs

--- lib/features/card/domain/repositories/card_repository_interface (copy).dart
+++
@@ -1,9 +0,0 @@
-import 'package:sixam_mart/features/card/domain/models/card_model.dart';
-
-abstract class CardRepositoryInterface{
-
-  Future<List<CardModel>> getAccountCards();
-  Future<void> saveAccountCard(CardModel card);
-  Future<void> deleteAccountCard(String nickname);
-
-}
--- lib/features/card/domain/services (copy)/card_service_interface.dart
+++
@@ -1,7 +0,0 @@
-import 'package:sixam_mart/features/card/domain/models/card_model.dart';
-
-abstract class CardServiceInterface {
-
-  Future<List<CardModel>> getAccountCardList();
-  Future<void> addCard(CardModel card); 
-}
