f19b951f7482e22ade0e9117133abbf1a0ab640c
giovaneneves7 <autornekoyasha@gmail.com>
2024-09-21 20:53:20 -0300

    dsldsmnf

--- lib/features/card/domain/services/card_brand_service.dart
+++ lib/features/card/domain/services/card_brand_service.dart
@@ -1,5 +1,5 @@
-import 'package/sixam_mart/features/card/services/card_brand_service_interface.dart';
-import 'package/sixam_mart/features/card/repositories/card_brand_repository_interface.dart';
+import 'package/sixam_mart/features/card/domain/services/card_brand_service_interface.dart';
+import 'package/sixam_mart/features/card/domain/repositories/card_brand_repository_interface.dart';
 import 'package:sixam_mart/features/card/domain/models/card_brand_model.dart';
 
 class CardBrandService implements CardBrandServiceInterface{
