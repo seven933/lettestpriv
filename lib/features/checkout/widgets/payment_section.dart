084fd8a35dbba4402979507286c8b0016a34bd32
giovaneneves7 <autornekoyasha@gmail.com>
2024-09-25 20:12:41 -0300

    mnsdk

--- lib/features/checkout/widgets/payment_section.dart
+++ lib/features/checkout/widgets/payment_section.dart
@@ -10,7 +10,6 @@ import 'package:sixam_mart/features/checkout/widgets/payment_method_bottom_sheet
 import 'package:sixam_mart/features/checkout/widgets/card_brand_selection_widget.dart';
 
 class PaymentSection extends StatelessWidget {
-
   final int? storeId;
   final bool isCashOnDeliveryActive;
   final bool isDigitalPaymentActive;
@@ -20,13 +19,13 @@ class PaymentSection extends StatelessWidget {
   final bool isOfflinePaymentActive;
 
   const PaymentSection({
-    super.key, 
-    this.storeId, 
-    required this.isCashOnDeliveryActive, 
+    super.key,
+    this.storeId,
+    required this.isCashOnDeliveryActive,
     required this.isDigitalPaymentActive,
-    required this.isWalletActive, 
-    required this.total, 
-    required this.checkoutController, 
+    required this.isWalletActive,
+    required this.total,
+    required this.checkoutController,
     required this.isOfflinePaymentActive,
   });
 
@@ -39,169 +38,174 @@ class PaymentSection extends StatelessWidget {
           children: [
             Text(storeId != null ? 'payment_method'.tr : 'choose_payment_method'.tr, style: robotoMedium),
 
-            storeId == null && !ResponsiveHelper.isDesktop(context) 
-              ? InkWell(
-                  onTap: () {
-                    Get.bottomSheet(
-                      PaymentMethodBottomSheet(
-                        isCashOnDeliveryActive: isCashOnDeliveryActive, 
-                        isDigitalPaymentActive: isDigitalPaymentActive,
-                        isWalletActive: isWalletActive, 
-                        storeId: storeId, 
-                        totalPrice: total, 
-                        isOfflinePaymentActive: isOfflinePaymentActive,
-                      ),
-                      backgroundColor: Colors.transparent, 
-                      isScrollControlled: true,
-                    );
-                  },
-                  child: Image.asset(Images.paymentSelect, height: 34, width: 54),
-                ) 
-              : const SizedBox(),
-          ]
+            storeId == null && !ResponsiveHelper.isDesktop(context)
+                ? InkWell(
+                    onTap: () {
+                      Get.bottomSheet(
+                        PaymentMethodBottomSheet(
+                          isCashOnDeliveryActive: isCashOnDeliveryActive,
+                          isDigitalPaymentActive: isDigitalPaymentActive,
+                          isWalletActive: isWalletActive,
+                          storeId: storeId,
+                          totalPrice: total,
+                          isOfflinePaymentActive: isOfflinePaymentActive,
+                        ),
+                        backgroundColor: Colors.transparent,
+                        isScrollControlled: true,
+                      );
+                    },
+                    child: Image.asset(Images.paymentSelect, height: 34, width: 54),
+                  )
+                : const SizedBox(),
+          ],
         ),
-        
+
         const Divider(),
 
-        // Use SingleChildScrollView to avoid overflow and ensure scrolling
         SingleChildScrollView(
           child: Column(
             children: [
               Container(
                 decoration: const BoxDecoration(),
                 padding: EdgeInsets.zero,
-                child: storeId != null 
-                  ? checkoutController.paymentMethodIndex == 0 
-                    ? Row(
-                        children: [
-                          Image.asset(
-                            Images.cash,
-                            width: 20, 
-                            height: 20,
-                            color: Theme.of(context).textTheme.bodyMedium!.color,
-                          ),
-                          const SizedBox(width: Dimensions.paddingSizeSmall),
-
-                          Expanded(
-                            child: Text(
-                              'cash_on_delivery'.tr,
-                              style: robotoMedium.copyWith(
-                                fontSize: Dimensions.fontSizeSmall, 
-                                color: Theme.of(context).disabledColor,
-                              ),
-                            ),
-                          ),
-
-                          Text(
-                            PriceConverter.convertPrice(total),
-                            textDirection: TextDirection.ltr,
-                            style: robotoMedium.copyWith(
-                              fontSize: Dimensions.fontSizeLarge, 
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                        ]
-                      ) 
-                    : const SizedBox()
-                  : InkWell(
-                      onTap: () {},
-                      child: Row(
-                        children: [
-                          checkoutController.paymentMethodIndex != -1 
-                            ? Image.asset(
-                                checkoutController.paymentMethodIndex == 0 ? Images.cash
-                                  : checkoutController.paymentMethodIndex == 1 ? Images.wallet
-                                  : checkoutController.paymentMethodIndex == 2 ? Images.pix 
-                                  : (checkoutController.paymentMethodIndex == 3 || checkoutController.paymentMethodIndex == 4) ? Images.cards
-                                  : Images.cash,
-                                width: 20, 
+                child: storeId != null
+                    ? checkoutController.paymentMethodIndex == 0
+                        ? Row(
+                            children: [
+                              Image.asset(
+                                Images.cash,
+                                width: 20,
                                 height: 20,
                                 color: Theme.of(context).textTheme.bodyMedium!.color,
-                              )
-                            : Icon(
-                                !ResponsiveHelper.isDesktop(context) 
-                                  ? Icons.wallet_outlined 
-                                  : Icons.add_circle_outline_sharp,
-                                size: 18, 
-                                color: !ResponsiveHelper.isDesktop(context)
-                                  ? Theme.of(context).disabledColor 
-                                  : Theme.of(context).primaryColor,
                               ),
-                          const SizedBox(width: Dimensions.paddingSizeSmall),
-
-                          Expanded(
-                            child: Row(
-                              children: [
-                                Text(
-                                  checkoutController.paymentMethodIndex == 0 
-                                    ? 'cash_on_delivery'.tr
-                                    : checkoutController.paymentMethodIndex == 1 
-                                      ? 'wallet_payment'.tr
-                                      : checkoutController.paymentMethodIndex == 2 
-                                        ? 'pix_on_delivery'.tr
-                                        : checkoutController.paymentMethodIndex == 3 
-                                          ? 'credit_card_on_delivery'.tr 
-                                          : checkoutController.paymentMethodIndex == 4 
-                                            ? 'debit_card_on_delivery'.tr
-                                            : 'select_payment_method'.tr,
+                              const SizedBox(width: Dimensions.paddingSizeSmall),
+                              Expanded(
+                                child: Text(
+                                  'cash_on_delivery'.tr,
                                   style: robotoMedium.copyWith(
                                     fontSize: Dimensions.fontSizeSmall,
-                                    color: !ResponsiveHelper.isDesktop(context) 
-                                      ? Theme.of(context).disabledColor
-                                      : checkoutController.paymentMethodIndex == -1 
-                                        ? Theme.of(context).primaryColor
-                                        : Theme.of(context).disabledColor,
+                                    color: Theme.of(context).disabledColor,
                                   ),
                                 ),
-                                checkoutController.paymentMethodIndex == 3 || checkoutController.paymentMethodIndex == 4 
-                                  ? Image.asset(
+                              ),
+                              Text(
+                                PriceConverter.convertPrice(total),
+                                textDirection: TextDirection.ltr,
+                                style: robotoMedium.copyWith(
+                                  fontSize: Dimensions.fontSizeLarge,
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                              ),
+                            ],
+                          )
+                        : const SizedBox()
+                    : InkWell(
+                        onTap: () {},
+                        child: Row(
+                          children: [
+                            checkoutController.paymentMethodIndex != -1
+                                ? Image.asset(
+                                    checkoutController.paymentMethodIndex == 0
+                                        ? Images.cash
+                                        : checkoutController.paymentMethodIndex == 1
+                                            ? Images.wallet
+                                            : checkoutController.paymentMethodIndex == 2
+                                                ? Images.pix
+                                                : (checkoutController.paymentMethodIndex == 3 ||
+                                                        checkoutController.paymentMethodIndex == 4)
+                                                    ? Images.cards
+                                                    : Images.cash,
+                                    width: 20,
+                                    height: 20,
+                                    color: Theme.of(context).textTheme.bodyMedium!.color,
+                                  )
+                                : Icon(
+                                    !ResponsiveHelper.isDesktop(context)
+                                        ? Icons.wallet_outlined
+                                        : Icons.add_circle_outline_sharp,
+                                    size: 18,
+                                    color: !ResponsiveHelper.isDesktop(context)
+                                        ? Theme.of(context).disabledColor
+                                        : Theme.of(context).primaryColor,
+                                  ),
+                            const SizedBox(width: Dimensions.paddingSizeSmall),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    checkoutController.paymentMethodIndex == 0
+                                        ? 'cash_on_delivery'.tr
+                                        : checkoutController.paymentMethodIndex == 1
+                                            ? 'wallet_payment'.tr
+                                            : checkoutController.paymentMethodIndex == 2
+                                                ? 'pix_on_delivery'.tr
+                                                : checkoutController.paymentMethodIndex == 3
+                                                    ? 'credit_card_on_delivery'.tr
+                                                    : checkoutController.paymentMethodIndex == 4
+                                                        ? 'debit_card_on_delivery'.tr
+                                                        : 'select_payment_method'.tr,
+                                    style: robotoMedium.copyWith(
+                                      fontSize: Dimensions.fontSizeSmall,
+                                      color: !ResponsiveHelper.isDesktop(context)
+                                          ? Theme.of(context).disabledColor
+                                          : checkoutController.paymentMethodIndex == -1
+                                              ? Theme.of(context).primaryColor
+                                              : Theme.of(context).disabledColor,
+                                    ),
+                                  ),
+                                  if (checkoutController.paymentMethodIndex == 3 ||
+                                      checkoutController.paymentMethodIndex == 4)
+                                    Image.asset(
                                       Images.mastercard,
                                       width: 25,
                                       height: 25,
-                                    )
-                                  : const SizedBox(),
-
-                                checkoutController.paymentMethodIndex == -1 && !ResponsiveHelper.isDesktop(context)
-                                  ? Padding(
+                                    ),
+                                  if (checkoutController.paymentMethodIndex == -1 &&
+                                      !ResponsiveHelper.isDesktop(context))
+                                    Padding(
                                       padding: const EdgeInsets.only(left: Dimensions.paddingSizeExtraSmall),
                                       child: Icon(Icons.error, size: 16, color: Theme.of(context).colorScheme.error),
-                                    )
-                                  : const SizedBox(),
-                              ],
+                                    ),
+                                ],
+                              ),
                             ),
-                          ),
+                            const SizedBox(height: 80),
 
-                          const SizedBox(height: 80), // Ensure spacing for widget positioning
-
-                          // Add the CardSelectionWidget inside a SizedBox to control its height
-                          checkoutController.paymentMethodIndex == 3 || checkoutController.paymentMethodIndex == 4
-                            ? SizedBox(
-                                height: 100, // Define an appropriate height
+                            // Ensure CardSelectionWidget has a proper size limit using ConstrainedBox
+                            if (checkoutController.paymentMethodIndex == 3 ||
+                                checkoutController.paymentMethodIndex == 4)
+                              ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  maxHeight: 150, // Limits the maximum height of CardSelectionWidget
+                                  minHeight: 50,
+                                ),
                                 child: CardSelectionWidget(
-                                  checkoutController: checkoutController, 
-                                  type: checkoutController.paymentMethodIndex, 
+                                  checkoutController: checkoutController,
+                                  type: checkoutController.paymentMethodIndex,
                                   storeId: storeId!,
                                 ),
                               )
-                            : const SizedBox(),
+                            else
+                              const SizedBox(),
 
-                          checkoutController.paymentMethodIndex != -1 
-                            ? PriceConverter.convertAnimationPrice(
+                            if (checkoutController.paymentMethodIndex != -1)
+                              PriceConverter.convertAnimationPrice(
                                 checkoutController.viewTotalPrice,
                                 textStyle: robotoMedium.copyWith(
-                                  fontSize: Dimensions.fontSizeLarge, 
+                                  fontSize: Dimensions.fontSizeLarge,
                                   color: Theme.of(context).primaryColor,
                                 ),
-                              ) 
-                            : const SizedBox(),
-                        ],
+                              )
+                            else
+                              const SizedBox(),
+                          ],
+                        ),
                       ),
-                    ),
               ),
             ],
           ),
         ),
-      ]
+      ],
     );
   }
 }
